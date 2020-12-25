61b2550b47e213095b63fb4ee8c8954d2350da08
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2020-12-25 14:25:06 +0900

    Active Storageの導入

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,7 @@
 class Prototype < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :users
+  # belong_to :
+  has_one_attached :image
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # has_many :comments
+  belongs_to :prototypes
 
   validates :name, presence: true
   validates :profile, presence: true
