b1c0fd472c7b0c405a160b745b281c9e1ab38196
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2020-12-30 19:09:53 +0900

    ユーザー表示情報の修正２

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   root to: "prototypes#index"
   resources :prototypes do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: :show
   #, only: [:index, :new, :create, :destory, :edit, :update, :show] 
